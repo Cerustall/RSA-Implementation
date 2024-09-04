864433c7efb9aea774e9cfad933340be5546f5da
Cerustall <edward.bailey.100@outlook.com>
2024-09-04 11:04:31 +0100

    Starting out

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use rand::prelude::*;
+
+type Prime = u128;
+
+fn random_prime() -> u128 {
+    let primes: [Prime; 10] = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29];
+    let mut rng = thread_rng();
+    let index = rng.gen_range(0..10);
+    return primes[index];
+}
+
 fn main() {
-    println!("Hello, world!");
+    let P  = random_prime(); 
+    let Q = random_prime();
+
+    println!("{}", example);
 }
