fc7cc68869bb82cd53f437f34a13118f7464b062
Cerustall <edward.bailey.100@outlook.com>
2024-09-04 10:34:11 +0100

    First commit

--- RSA.rs
+++ RSA.rs
@@ -1,3 +1,16 @@
+use rand::prelude::*;
+
+type Prime = u128;
+
+fn random_prime() -> Prime{
+    let primes [Prime, 10] = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29];
+    let mut rnd = thread_rng();
+    let index = rnd.gen_range(0.0..10.0);
+    primes[index as u64];
+}
+
 fn main() {
-    
+    let mut example  = random_prime(); 
+
+    println!("{}" example);
 }
