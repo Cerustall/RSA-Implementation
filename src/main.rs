fe72688045aa2d82814090dbf288464b1d81be4a
Cerustall <edward.bailey.100@outlook.com>
2024-09-07 22:20:22 +0100

    same as before

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use rand::prelude::*;
 
 type Prime = u128;
 
-fn random_prime() -> u128 {
+fn random_prime() -> Prime {
     let primes: [Prime; 10] = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29];
     let mut rng = thread_rng();
     let index = rng.gen_range(0..10);
@@ -15,7 +15,7 @@ fn main() {
     let q = random_prime();
     let n = p*q;
     let theta_of_n = (p-1)*(q-1);
-    let mut e = 0;
+    let mut e;
     let k = rng.gen_range(0..340282366920938463463374607431768211455);
     let public_key;
     let private_key;
@@ -24,13 +24,15 @@ fn main() {
         e = rng.gen_range(0..100000);
 
         if (theta_of_n % e != 0) && (e > 1) && (e < theta_of_n){
-            break;
+            if n.checked_pow(e as u32).is_some(){
+                break;
+            }
         }else{
             continue;
         }
     }
 
-    public_key = n.checked_pow(e as u32);
+    public_key = n.checked_pow(e as u32).unwrap();
     private_key = (k * theta_of_n + 1) / e;
 
     println!("Public key: {}", public_key);
