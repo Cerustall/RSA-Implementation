6e54acc56d1a28a588fe20b61c63fb49148b45d2
Cerustall <edward.bailey.100@outlook.com>
2024-09-12 09:04:08 +0100

    hand in day

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,14 @@
 use rand::prelude::*;
+use std::io;
 
 type Prime = u128;
+
+struct Keys{
+    private: u128,
+    public: u128,
+    n: u128,
+    exponent: u128,
+}
 
 fn random_prime() -> Prime {
     let primes: [Prime; 10] = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29];
@@ -9,7 +17,13 @@ fn random_prime() -> Prime {
     return primes[index];
 }
 
-fn main() {
+fn gen_keys() -> Keys{
+    let mut keys = Keys{
+        private: 0,
+        public: 0,
+        n: 0,
+        exponent: 0,
+    };
     let mut rng: ThreadRng = thread_rng();
     let p: u128  = random_prime(); 
     let q: u128 = random_prime();
@@ -17,11 +31,9 @@ fn main() {
     let theta_of_n: u128 = (p-1)*(q-1);
     let mut e: u128;
     let k: u128 = rng.gen_range(0..100000000000000000);
-    let public_key: u128;
-    let private_key: u128;
 
     loop{
-        e = rng.gen_range(0..1000);
+        e = rng.gen_range(1..1000);
 
         if (theta_of_n % e != 0) && (e > 1) && (e < theta_of_n){
             if n.checked_pow( e as u32).is_some(){
@@ -31,10 +43,50 @@ fn main() {
             continue;
         }
     }
+    keys.public = n.checked_pow(e as u32).unwrap();
+    keys.private = (k * theta_of_n + 1) / e;
+    keys.n = n;
+    keys.exponent = e;
+    keys
+}
 
-    public_key = n.checked_pow(e as u32).unwrap();
-    private_key = (k * theta_of_n + 1) / e;
+fn get_plaintext() -> String {
+    let mut input: String = String::new();
+    println!("Please enter your text: ");
+    io::stdin().read_line(&mut input).expect("Failed to read text");
+    input
+}
 
-    println!("Public key: {}", public_key);
-    println!("Private key: {}", private_key);
+fn encrypt(plaintext: String, exponent: u128, n:u128) -> String {
+    let mut ciphertext: String = String::new();
+
+    for char in plaintext.chars(){
+        ciphertext.push(((((char as u128).checked_pow(exponent as u32)).unwrap() % n) as u8 as char));
+    }
+    ciphertext
+}
+
+fn decrypt(ciphertext: String, private_key: u128, n: u128) -> String {
+    let mut plaintext: String = String::new();
+
+    for char in ciphertext.chars(){
+        plaintext.push(((char as u128).checked_pow(private_key as u32).unwrap() % n)as u32 as char);
+    }
+
+    plaintext
+}
+
+fn main() {
+    let keys: Keys = gen_keys();
+    let plaintext: String = get_plaintext();
+
+    println!("Input: {}", plaintext);
+
+    let ciphertext = encrypt(plaintext, keys.exponent, keys.n);
+
+    println!("Ciphertext: {}", ciphertext);
+
+    let output = decrypt(ciphertext, keys.private, keys.n);
+
+    println!("Decrypted text: {}", output);
 }
